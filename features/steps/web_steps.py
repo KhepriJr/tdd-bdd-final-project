a980b8fda8545073ef9cdbb204cff3d6f181168f
Kheprijr <matthewsben91@gmail.com>
2024-03-15 14:41:37 -0400

    I did it

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -125,6 +125,7 @@ def step_impl(context, name):
     
 @then('I should see the message "{message}"')
 def step_impl(context, message):
+    print(context)
     found = WebDriverWait(context.driver, context.wait_seconds).until(
         expected_conditions.text_to_be_present_in_element(
             (By.ID, 'flash_message'),
